f1a02f9ce3417a5384b64020da68fbac9b0154aa
alaksandar_km <ebenzomarket@gmail.com>
2021-03-11 01:18:27 +0100

    add localStorage

--- src/Actions.js
+++ src/Actions.js
@@ -1,6 +1,7 @@
 import { NotImportantTask } from "./notImportantTask.js";
 import { ImportantTask } from "./importantTask.js";
 import { VeryImportantTask } from "./veryImportantTask.js";
+import { Task } from "./task.js";
 
 import { notImportantStore, importantStore, veryImportantStore } from "./store.js";
 
@@ -16,7 +17,7 @@ export function openForm(addButton) {
     const addList = document.querySelector("#add-list");
     addList.classList.remove("close");
     addList.classList.add("open");
-    
+
     addButton.hidden = true;
 }
 
@@ -47,7 +48,8 @@ export function closeForm(close_form) {
 // Создать задачу при клике на кнопку "Добавить" на основе конструктора из "./task.js",
 // поместить задачу в соответствующей колонке, если:
 // 1) поле ввода не пустое, 2) выбран уровень срочности;
-// блокировка ввода дублирующихся задач:
+// findDublicateTask() - блокировка ввода дублирующихся задач;
+// changeLocalStorage() - сохранение изменений в LocalStorage;
 
 export function submitTask(e) {
 
@@ -58,30 +60,36 @@ export function submitTask(e) {
 
     const li = document.createElement("li");
 
-    const {value} = inputText;
+    const { value } = inputText;
 
     if (value === "" || findDublicateTask(value)) {
         return
     }
 
 
-    if (options[0].selected && (inputText.value != 0)) {
+    if (options[0].selected && (inputText.value != null)) {
 
         new NotImportantTask(inputText.value).create(li);
         inputText.value = "";
         options[0].selected = false;
 
-    } else if (options[1].selected && (inputText.value != 0)) {
+        changeLocalStorage("notImportantStore", notImportantStore);
+
+    } else if (options[1].selected && (inputText.value != null)) {
 
         new ImportantTask(inputText.value).create(li);
         inputText.value = "";
         options[1].selected = false;
 
-    } else if (options[2].selected && (inputText.value != 0)) {
+        changeLocalStorage("importantStore", importantStore);
+
+    } else if (options[2].selected && (inputText.value != null)) {
 
         new VeryImportantTask(inputText.value).create(li);
         inputText.value = "";
         options[2].selected = false;
+
+        changeLocalStorage("veryImportantStore", veryImportantStore);
     }
 }
 
@@ -125,13 +133,19 @@ export function markTask(e) {
 
 
         if (taskTypeName === "notImportant") {
+
             changeStoreChecked(notImportantStore, taskType, taskTypeName, e.target.checked);
+            changeLocalStorage("notImportantStore", notImportantStore);
 
         } else if (taskTypeName === "important") {
+
             changeStoreChecked(importantStore, taskType, taskTypeName, e.target.checked);
+            changeLocalStorage("importantStore", importantStore);
 
         } else if (taskTypeName === "veryImportant") {
+
             changeStoreChecked(veryImportantStore, taskType, taskTypeName, e.target.checked);
+            changeLocalStorage("veryImportantStore", veryImportantStore);
         }
     }
 }
@@ -155,30 +169,36 @@ export function deleteTask(e) {
         e.target.closest("li").firstElementChild.checked === true) {
 
         let taskType = e.target.parentElement.dataset;
-        
+
         let taskTypeName = Object.keys(taskType)[0];
 
         e.target.closest("li").remove();
 
         if (taskTypeName === "notImportant") {
-            
+
             notImportantStore.splice(e.target.parentElement.dataset.notImportant, 1);
+
+            changeLocalStorage("notImportantStore", notImportantStore);
             culculateListAtributes(notImportantStore, "data-not-important");
 
         } else if (taskTypeName === "important") {
-            
+
             importantStore.splice(e.target.parentElement.dataset.important, 1);
+
             culculateListAtributes(importantStore, "data-important");
+            changeLocalStorage("importantStore", importantStore);
+
 
         } else if (taskTypeName === "veryImportant") {
-            
+
             veryImportantStore.splice(e.target.parentElement.dataset.veryImportant, 1);
+
             culculateListAtributes(veryImportantStore, "data-very-important");
+            changeLocalStorage("veryImportantStore", veryImportantStore);
         }
-
     } else return;
-
 }
+
 
 
 // Переопределить data-attribute и id всех задач при удалении одной из них: 
@@ -186,7 +206,7 @@ export function deleteTask(e) {
 function culculateListAtributes(store, attribute) {
 
     const list = document.querySelectorAll(`[${attribute}]`);
-    
+
     for (let i in store) {
         store[i].id = i;
         list[i].setAttribute(attribute, i);
@@ -207,5 +227,68 @@ function findDublicateTask(name) {
         return false;
     } else {
         return true;
+    }
+}
+
+
+
+// Сохранение изменений в LocalStorage при создании, маркировке и удалении задач;
+function changeLocalStorage(name, store) {
+    localStorage.setItem(name, JSON.stringify(store));
+}
+
+
+
+// Извлечение данных из localStorage при загрузке страницы:
+
+export function reload() {
+
+
+    if (localStorage.getItem("notImportantStore")) {
+
+        notImportantStore.push(...JSON.parse(localStorage.getItem("notImportantStore")));
+
+        for (let i in notImportantStore) {
+
+            const li = document.createElement("li");
+            li.setAttribute("data-not-important", i);
+
+            new Task(notImportantStore[i].name, notImportantStore[i].checked).create(li);
+
+            const ul = document.querySelector(".not-important-task-col ul");
+            ul.append(li);
+        }
+    }
+
+    if (localStorage.getItem("importantStore")) {
+
+        importantStore.push(...JSON.parse(localStorage.getItem("importantStore")));
+
+        for (let i in importantStore) {
+
+            const li = document.createElement("li");
+            li.setAttribute("data-important", i);
+
+            new Task(importantStore[i].name, importantStore[i].checked).create(li);
+
+            const ul = document.querySelector(".important-task-col ul");
+            ul.append(li);
+        }
+    }
+
+    if (localStorage.getItem("veryImportantStore")) {
+
+        veryImportantStore.push(...JSON.parse(localStorage.getItem("veryImportantStore")));
+
+        for (let i in veryImportantStore) {
+
+            const li = document.createElement("li");
+            li.setAttribute("data-very-important", i);
+
+            new Task(veryImportantStore[i].name, veryImportantStore[i].checked).create(li);
+
+            const ul = document.querySelector(".very-important-task-col ul");
+            ul.append(li);
+        }
     }
 }
